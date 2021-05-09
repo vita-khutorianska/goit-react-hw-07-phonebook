8e6eb553903c2704d5b2a1cc745d75fbd514e927
Vita Khutorianska <steblvita@gmail.com>
2021-05-09 22:56:44 +0300

    add operations

---
+++ src/Redux/Phonebook/operationsApi.js
@@ -0,0 +1,47 @@
+import {
+  getContactsRequest,
+  getContactsSuccess,
+  getContactsFailure,
+  addContactsRequest,
+  addSuccess,
+  addFailure,
+  deleteContactsRequest,
+  deleteChangeSuccess,
+  deleteChangeFailure,
+} from './phone-actions';
+// * as actions
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:4040';
+
+export const getContact = () => async dispatch => {
+  dispatch(getContactsRequest());
+  try {
+    const { data } = await axios.get('/contacts');
+
+    dispatch(getContactsSuccess(data));
+  } catch (error) {
+    dispatch(getContactsFailure(error));
+  }
+  // axios
+  //   .get("/contacts")
+  //   .then(({ data }) => dispatch(getContactsSuccess(data)))
+  //   .catch((err) => dispatch(getContactsFailure(err)));
+};
+
+export const addContact = ({ name, number }) => (dispatch, getState) => {
+  dispatch(addContactsRequest());
+
+  axios
+    .post('/contacts', { name, number })
+    .then(({ data }) => dispatch(addSuccess(data)))
+    .catch(err => dispatch(addFailure(err)));
+};
+export const deleteContact = contactId => dispatch => {
+  dispatch(deleteContactsRequest());
+
+  axios
+    .delete(`/contacts/${contactId}`)
+    .then(() => dispatch(deleteChangeSuccess(contactId)))
+    .catch(error => dispatch(deleteChangeFailure(error)));
+};
